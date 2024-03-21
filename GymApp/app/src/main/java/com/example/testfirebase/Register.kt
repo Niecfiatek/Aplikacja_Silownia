4b097a3805d0d29fe2c31bffba059f15ec8802c8
NiecfiateQ <bartosz.niedzwiedzki02@gmail.com>
2024-03-21 09:36:17 +0100

    Poprawki

--- GymApp/app/src/main/java/com/example/testfirebase/Register.kt
+++ GymApp/app/src/main/java/com/example/testfirebase/Register.kt
@@ -1,12 +1,27 @@
 package com.example.testfirebase
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
+
+    lateinit var editTextMail:TextInputEditText
+    lateinit var editTextPassword:TextInputEditText
+    lateinit var buttonReg:Button
+    lateinit var auth:FirebaseAuth
+    lateinit var progressBar:ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +30,40 @@ class Register : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        auth = FirebaseAuth.getInstance()
+        editTextMail = findViewById(R.id.email)
+        editTextPassword = findViewById(R.id.password)
+        buttonReg = findViewById(R.id.btn_register)
+        progressBar = findViewById(R.id.progressBar)
+        buttonReg.setOnClickListener{
+            fun onClick(view:View){
+                progressBar.visibility = View.VISIBLE
+                var email:String
+                var password:String
+                email = editTextMail.getText().toString()
+                password = editTextMail.getText().toString()
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show()
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show()
+                }
+
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener() { task ->
+                        progressBar.visibility = View.GONE
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(baseContext, "Account created", Toast.LENGTH_SHORT).show()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
         }
     }
 }
