f7f20e91e95a444e2d3e61da965c5a1863c69b8b
NiecfiateQ <bartosz.niedzwiedzki02@gmail.com>
2024-04-09 23:01:18 +0200

    Rozbudowanie dodawania cwiczenia

--- GymApp/app/src/main/java/com/example/testfirebase/AddExercise.kt
+++ GymApp/app/src/main/java/com/example/testfirebase/AddExercise.kt
@@ -1,5 +1,6 @@
 package com.example.testfirebase
 
+import android.content.ClipDescription
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -22,6 +23,10 @@ class AddExercise : AppCompatActivity() {
     private lateinit var back: Button
     private lateinit var textView: TextView
     private lateinit var nameInput: EditText
+    private lateinit var bodyPartInput: EditText
+    private lateinit var bodySubPartInput: EditText
+    private lateinit var type: EditText
+    private lateinit var description: EditText
     private val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,27 +41,38 @@ class AddExercise : AppCompatActivity() {
         back = findViewById(R.id.back)
         textView = findViewById(R.id.name)
         nameInput = findViewById(R.id.nameInput)
-
+        bodyPartInput = findViewById(R.id.partInput)
+        bodySubPartInput = findViewById(R.id.subpartInput)
+        type = findViewById(R.id.typeInput)
+        description = findViewById(R.id.descriptionInput)
 
 
         add.setOnClickListener {
 
             val n = nameInput.text.toString().trim()
-            val exer = hashMapOf("Name of Exercise" to n)
+            val p = bodyPartInput.text.toString().trim()
+            val sp = bodySubPartInput.text.toString().trim()
+            val t = type.text.toString().trim()
+            val d = description.text.toString().trim()
+            val exer = hashMapOf(
+                "Name of Exercise" to n,
+                "Body part" to p,
+                "Body sub-part" to sp,
+                "Type of training" to t,
+                "Description" to d
+            )
 
             db.collection("Exercise").document().set(exer)
                 .addOnSuccessListener {
                     Toast.makeText(this, "Successfully Added!", Toast.LENGTH_SHORT).show()
                     nameInput.text.clear()
-                    //addExerciseToFirestore(name)
-                    //val intent = Intent(applicationContext, AddExercise::class.java).apply {
-                    //    flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                //  }
-                //startActivity(intent)
+                    bodyPartInput.text.clear()
+                    bodySubPartInput.text.clear()
+                    type.text.clear()
+                    description.text.clear()
                 }
                 .addOnFailureListener {
                     Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show()
-                    nameInput.text.clear()
                 }
         }
 
@@ -67,9 +83,4 @@ class AddExercise : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
-    //val intent = Intent(applicationContext, AnotherActivity::class.java).apply {
-    //    flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-    //}
-
 }
