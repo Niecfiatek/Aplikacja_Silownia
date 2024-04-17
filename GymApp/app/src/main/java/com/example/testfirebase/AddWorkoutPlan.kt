84291b599e297decaea86df47f2f389cefadb6ef
NiecfiateQ <bartosz.niedzwiedzki02@gmail.com>
2024-04-17 23:26:03 +0200

    Dodanie add plan

--- GymApp/app/src/main/java/com/example/testfirebase/AddWorkoutPlan.kt
+++ GymApp/app/src/main/java/com/example/testfirebase/AddWorkoutPlan.kt
@@ -7,24 +7,32 @@ import android.widget.Button
 import android.text.InputType
 import android.util.TypedValue
 import android.widget.LinearLayout
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.marginBottom
+import androidx.core.view.marginEnd
+import androidx.core.view.marginStart
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class AddWorkoutPlan : AppCompatActivity() {
-    private val firestore: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
-    private val exerciseCollection = firestore.collection("Exercise")
-    private lateinit var firstExercise: AutoCompleteTextView
+    private val db = Firebase.firestore
+    private val exerciseCollection = db.collection("Exercise")
     private lateinit var workoutPlanInputContainer: LinearLayout
     private lateinit var addEx: Button
     private lateinit var addWorkPlan: Button
     private lateinit var removeEx: Button
-    val exerciseNames = mutableListOf<String>()
+    private lateinit var firstSpinner: Spinner
+    private lateinit var adapter: ArrayAdapter<String>
+    private lateinit var listofExercise: MutableList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,51 +48,85 @@ class AddWorkoutPlan : AppCompatActivity() {
         addEx = findViewById(R.id.addExercise)
         addWorkPlan = findViewById(R.id.addPlan)
         removeEx = findViewById(R.id.removeExercise)
-        firstExercise = findViewById(R.id.NameOfExerciseInput)
+        firstSpinner = findViewById(R.id.exerciseSpinner)
+
         val exerciseNamesTask: Task<QuerySnapshot> = exerciseCollection.get()
         exerciseNamesTask.addOnSuccessListener { querySnapshot ->
+            val exerciseNames = mutableListOf<String>()
             for (document in querySnapshot.documents) {
                 val exerciseName = document.getString("Name of Exercise")
                 exerciseName?.let {
                     exerciseNames.add(it)
                 }
             }
-            val adapter = ArrayAdapter(this, R.layout.dropdown_item, exerciseNames)
-            firstExercise.setAdapter(adapter)
+            adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, exerciseNames)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            firstSpinner.adapter = adapter
         }
 
         addEx.setOnClickListener {
-            addAutoCompleteTextView(exerciseNames)
+            addSpinner()
         }
         removeEx.setOnClickListener{
-            removeAutoCompleteTextView()
+            removeSpinner()
+        }
+        addWorkPlan.setOnClickListener{
+            addWorkPlan()
         }
 
     }
 
-    private fun addAutoCompleteTextView(array: MutableList<String>) {
-        val textInputLayout = TextInputLayout(this)
-        val autoCompleteTextView = AutoCompleteTextView(this)
-        autoCompleteTextView.layoutParams = LinearLayout.LayoutParams(
+    private fun addSpinner() {
+        val newSpinner = Spinner(this)
+        newSpinner.adapter = adapter
+        // Kopiowanie ustawień pierwszego Spinnera
+        newSpinner.background = firstSpinner.background
+        newSpinner.prompt = firstSpinner.prompt
+        newSpinner.setSelection(firstSpinner.selectedItemPosition)
+
+        // Pobranie marginesów z pierwszego Spinnera
+        val params = firstSpinner.layoutParams as LinearLayout.LayoutParams
+
+        // Ustawienie marginesów dla nowego Spinnera
+        val newParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
-        autoCompleteTextView.inputType = InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
-        autoCompleteTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
-        autoCompleteTextView.setHint("Exercise Name")
-        textInputLayout.addView(autoCompleteTextView)
-
-        // Ustawienie stylu dla TextInputLayout, aby odpowiadał pierwszemu TextInputLayout
-        textInputLayout.setHintTextAppearance(com.google.android.material.R.style.Widget_Material3_TextInputLayout_OutlinedBox_ExposedDropdownMenu)
-
-        workoutPlanInputContainer.addView(textInputLayout)
-        val adapter = ArrayAdapter(this, R.layout.dropdown_item, array)
-        autoCompleteTextView.setAdapter(adapter)
+        newParams.marginStart = params.marginStart
+        newParams.marginEnd = params.marginEnd
+        newSpinner.layoutParams = newParams
+        workoutPlanInputContainer.addView(newSpinner)
     }
-    private fun removeAutoCompleteTextView() {
+
+    private fun removeSpinner() {
         if (workoutPlanInputContainer.childCount > 1) {
             val lastIndex = workoutPlanInputContainer.childCount - 1
             workoutPlanInputContainer.removeViewAt(lastIndex)
+        }
+    }
+    private fun addWorkPlan() {
+        //listofExercise.clear() // Wyczyść listę przed dodaniem nowych ćwiczeń
+        for (i in 0 until workoutPlanInputContainer.childCount - 1) {
+            val view = workoutPlanInputContainer.getChildAt(i)
+            if (view is Spinner) {
+                val selectedExercise = view.selectedItem as String
+                listofExercise.add(selectedExercise)
+            }
+        }
+
+        // Przypisanie danych do bazy danych dla każdego ćwiczenia osobno
+        listofExercise.forEach { exercise ->
+            val workoutPlan = hashMapOf(
+                "Exercise" to exercise
+            )
+            // Dodanie danych do bazy danych
+            db.collection("WorkoutPlans").add(workoutPlan)
+                .addOnSuccessListener {
+                    Toast.makeText(this@AddWorkoutPlan, "Successfully Added!", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this@AddWorkoutPlan, "Failed!", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
