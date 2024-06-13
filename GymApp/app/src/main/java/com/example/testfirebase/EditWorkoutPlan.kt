7848139818ec6ef9b2449fddb402827a0017c0be
Piotr Warnowski <162305236+PKWPiotrekwar@users.noreply.github.com>
2024-06-13 21:17:34 +0200

    naprawy do editWorkout i małe zmiany w active exercise

--- GymApp/app/src/main/java/com/example/testfirebase/EditWorkoutPlan.kt
+++ GymApp/app/src/main/java/com/example/testfirebase/EditWorkoutPlan.kt
@@ -82,14 +82,16 @@ class EditWorkoutPlan : AppCompatActivity() {
 
         buttonSave.setOnClickListener {
             val workoutName = workoutSpinner.selectedItem.toString()
-            val newExercises = mutableListOf<String>()
+            val newExercises = mutableListOf<Pair<String, String>>()
 
             // Pobierz ćwiczenia z każdego spinnera i dodaj do listy
-            for (i in 0 until layoutForSpinners.childCount) {
+            for (i in 0 until layoutForSpinners.childCount step 2) {
                 val spinner = layoutForSpinners.getChildAt(i) as? Spinner
+                val editText = layoutForSpinners.getChildAt(i + 1) as? EditText
                 val selectedExercise = spinner?.selectedItem?.toString()
-                selectedExercise?.let {
-                    newExercises.add(it)
+                val exerciseValue = editText?.text?.toString()
+                if (selectedExercise != null && exerciseValue != null) {
+                    newExercises.add(Pair(selectedExercise, exerciseValue))
                 }
             }
 
@@ -97,11 +99,14 @@ class EditWorkoutPlan : AppCompatActivity() {
             val newWorkoutName = editText.text.toString()
             updateWorkoutNameAndPlanInDatabase(workoutName, newWorkoutName, newExercises)
 
-            recreate()
+            // Odśwież zawartość spinnera
+            refreshWorkoutSpinner()
         }
 
+
+
         workoutSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            var previousSpinners = mutableListOf<Spinner>()
+            var previousViews = mutableListOf<View>()
 
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -109,11 +114,11 @@ class EditWorkoutPlan : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-                // Usuń poprzednie spinnery
-                previousSpinners.forEach { spinner ->
-                    layoutForSpinners.removeView(spinner)
+                // Usuń poprzednie widoki
+                previousViews.forEach { view ->
+                    layoutForSpinners.removeView(view)
                 }
-                previousSpinners.clear()
+                previousViews.clear()
 
                 val selectedItem = adapter.getItem(position)
                 selectedItem?.let { workoutName ->
@@ -125,6 +130,7 @@ class EditWorkoutPlan : AppCompatActivity() {
                     query.get().addOnSuccessListener { documents ->
                         if (!documents.isEmpty) {
                             val document = documents.documents[0]
+                            val exercisesMap = document.get("Exercises") as? Map<String, String>
                             val exercisesList = mutableListOf<String>()
                             for ((key, value) in document.data.orEmpty()) {
                                 if (key.startsWith("Exercise")) {
@@ -136,7 +142,7 @@ class EditWorkoutPlan : AppCompatActivity() {
                             }
 
                             // Inicjalizacja Spinnera dla każdego ćwiczenia
-                            exercisesList.forEach { exerciseName ->
+                            exercisesMap?.forEach { (exerciseName, exerciseValue) ->
                                 val spinner = Spinner(this@EditWorkoutPlan)
                                 val exerciseNamesTask: Task<QuerySnapshot> = exerciseCollection.get()
 
@@ -157,11 +163,16 @@ class EditWorkoutPlan : AppCompatActivity() {
                                     spinner.setSelection(defaultPosition)
                                 }
 
-                                // Dodanie Spinnera do layoutu
-                                layoutForSpinners.addView(spinner)
+                                val editText = EditText(this@EditWorkoutPlan)
+                                editText.setText(exerciseValue)
 
-                                // Dodaj spinner do listy poprzednich spinnerów
-                                previousSpinners.add(spinner)
+                                // Dodanie Spinnera i EditText do layoutu
+                                layoutForSpinners.addView(spinner)
+                                layoutForSpinners.addView(editText)
+
+                                // Dodaj spinner do listy poprzednich widoków
+                                previousViews.add(spinner)
+                                previousViews.add(editText)
                             }
                         } else {
                             Toast.makeText(this@EditWorkoutPlan, "Brak danych dla wybranego planu treningowego", Toast.LENGTH_SHORT).show()
@@ -197,6 +208,28 @@ class EditWorkoutPlan : AppCompatActivity() {
         }
     }
 
+    private fun refreshWorkoutSpinner() {
+        val workoutsNamesTask: Task<QuerySnapshot> = workoutCollection.get()
+
+        workoutsNamesTask.addOnSuccessListener { querySnapshot ->
+            val workoutNames = mutableListOf<String>()
+            for (document in querySnapshot.documents) {
+                val workoutName = document.getString("Name of Workout Plan")
+                workoutName?.let {
+                    workoutNames.add(it)
+                }
+            }
+            adapter.clear()
+            adapter.addAll(workoutNames)
+            adapter.notifyDataSetChanged()
+        }
+    }
+    private fun scrollToBottom(scrollView: ScrollView) {
+        scrollView.post {
+            scrollView.fullScroll(View.FOCUS_DOWN)
+        }
+    }
+
     private fun showDeleteConfirmationDialog(workoutName: String) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Deleting Workout...")
@@ -217,25 +250,15 @@ class EditWorkoutPlan : AppCompatActivity() {
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        // Zapisanie indeksu wybranego elementu w spinnerze
+        // Zapisanie indeksu wybranego elementu spinnera do savedInstanceState
         outState.putInt("selectedWorkoutIndex", workoutSpinner.selectedItemPosition)
         super.onSaveInstanceState(outState)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-
-        // Przywrócenie zapamiętanego indeksu wybranego elementu w spinnerze
         selectedWorkoutIndex = savedInstanceState.getInt("selectedWorkoutIndex", 0)
-    }
-
-    private fun removeSelectedSpinner() {
-        val lastSpinnerIndex = layoutForSpinners.childCount - 1
-        if (lastSpinnerIndex >= 0) {
-            layoutForSpinners.removeViewAt(lastSpinnerIndex)
-        } else {
-            Toast.makeText(this, "No spinner to remove", Toast.LENGTH_SHORT).show()
-        }
+        workoutSpinner.setSelection(selectedWorkoutIndex)
     }
 
     private fun addNewExerciseSpinner() {
@@ -253,83 +276,86 @@ class EditWorkoutPlan : AppCompatActivity() {
             val exerciseAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, exerciseNames)
             exerciseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = exerciseAdapter
-
-            // Ustawienie parametrów layoutu dla spinnera
-            val layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-
-            // Dodanie spinnera na końcu layoutu
-            layoutForSpinners.addView(spinner, layoutParams)
-        }.addOnFailureListener { exception ->
-            Toast.makeText(this, "Error retrieving exercise data: ${exception.message}", Toast.LENGTH_SHORT).show()
         }
+
+        val editText = EditText(this)
+
+        layoutForSpinners.addView(spinner)
+        layoutForSpinners.addView(editText)
+
+        scrollToBottom(findViewById(R.id.scrollView))
     }
 
-    private fun updateWorkoutNameAndPlanInDatabase(workoutName: String, newWorkoutName: String, newExercises: List<String>) {
-        val query = workoutCollection.whereEqualTo("Name of Workout Plan", workoutName)
+
+    private fun removeSelectedSpinner() {
+        val childCount = layoutForSpinners.childCount
+        if (childCount > 1) {
+            layoutForSpinners.removeViewAt(childCount - 1)
+            layoutForSpinners.removeViewAt(childCount - 2)
+        }
+
+        scrollToBottom(findViewById(R.id.scrollView))
+    }
+
+
+    private fun updateWorkoutNameAndPlanInDatabase(
+        oldWorkoutName: String,
+        newWorkoutName: String,
+        exercises: List<Pair<String, String>>
+    ) {
+        val query = workoutCollection.whereEqualTo("Name of Workout Plan", oldWorkoutName)
         query.get().addOnSuccessListener { documents ->
             if (!documents.isEmpty) {
                 val document = documents.documents[0]
-                val workoutId = document.id
-
-                val updatedData = mutableMapOf<String, Any>()
-
-                // Zaktualizuj nazwę planu treningowego
-                updatedData["Name of Workout Plan"] = newWorkoutName
-
-                // Usuń wszystkie obecne ćwiczenia
-                for ((key, _) in document.data.orEmpty()) {
-                    if (key.startsWith("Exercise")) {
-                        updatedData[key] = FieldValue.delete()
-                    }
-                }
-
-                // Dodaj nowe ćwiczenia
-                newExercises.forEachIndexed { index, exercise ->
-                    updatedData["Exercise ${index + 1}"] = exercise
-                }
-
-                // Zaktualizuj dokument w bazie danych
-                workoutCollection.document(workoutId)
-                    .update(updatedData)
+                workoutCollection.document(document.id).delete()
                     .addOnSuccessListener {
-                        Toast.makeText(this@EditWorkoutPlan, "Workout plan updated successfully", Toast.LENGTH_SHORT).show()
-                        workoutSpinner.setSelection(0)
+                        // Utwórz nowy dokument z nową nazwą i ćwiczeniami
+                        val newWorkoutPlan = hashMapOf(
+                            "Name of Workout Plan" to newWorkoutName,
+                            "Exercises" to exercises.toMap()
+                        )
+                        workoutCollection.add(newWorkoutPlan)
+                            .addOnSuccessListener {
+                                Toast.makeText(this, "Workout plan updated successfully", Toast.LENGTH_SHORT).show()
+                                refreshWorkoutSpinner()
+                            }
+                            .addOnFailureListener { exception ->
+                                Toast.makeText(this, "Error adding new workout plan: ${exception.message}", Toast.LENGTH_SHORT).show()
+                            }
                     }
                     .addOnFailureListener { exception ->
-                        Toast.makeText(this@EditWorkoutPlan, "Error updating workout plan: ${exception.message}", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "Error deleting old workout plan: ${exception.message}", Toast.LENGTH_SHORT).show()
                     }
             } else {
-                Toast.makeText(this@EditWorkoutPlan, "Workout plan not found", Toast.LENGTH_SHORT).show()
+                // Dokument nie istnieje, więc nie ma potrzeby go usuwać
+                // Utwórz nowy dokument z nową nazwą i ćwiczeniami
+                val newWorkoutPlan = hashMapOf(
+                    "Name of Workout Plan" to newWorkoutName,
+                    "Exercises" to exercises.toMap()
+                )
+                workoutCollection.add(newWorkoutPlan)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Workout plan created successfully", Toast.LENGTH_SHORT).show()
+                        refreshWorkoutSpinner()
+                    }
+                    .addOnFailureListener { exception ->
+                        Toast.makeText(this, "Error adding new workout plan: ${exception.message}", Toast.LENGTH_SHORT).show()
+                    }
             }
         }.addOnFailureListener { exception ->
-            Toast.makeText(this@EditWorkoutPlan, "Error retrieving workout plan data: ${exception.message}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Error fetching old workout plan: ${exception.message}", Toast.LENGTH_SHORT).show()
         }
     }
+
 
     private fun deleteWorkoutPlanFromDatabase(workoutName: String) {
         val query = workoutCollection.whereEqualTo("Name of Workout Plan", workoutName)
         query.get().addOnSuccessListener { documents ->
-            if (!documents.isEmpty) {
-                val document = documents.documents[0]
-                val workoutId = document.id
-
-                // Usuń dokument z bazy danych
-                workoutCollection.document(workoutId)
-                    .delete()
-                    .addOnSuccessListener {
-                        Toast.makeText(this@EditWorkoutPlan, "Workout plan deleted successfully", Toast.LENGTH_SHORT).show()
-                    }
-                    .addOnFailureListener { exception ->
-                        Toast.makeText(this@EditWorkoutPlan, "Error deleting workout plan: ${exception.message}", Toast.LENGTH_SHORT).show()
-                    }
-            } else {
-                Toast.makeText(this@EditWorkoutPlan, "Workout plan not found", Toast.LENGTH_SHORT).show()
+            for (document in documents) {
+                workoutCollection.document(document.id).delete()
             }
         }.addOnFailureListener { exception ->
-            Toast.makeText(this@EditWorkoutPlan, "Error retrieving workout plan data: ${exception.message}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Błąd podczas usuwania planu treningowego: ${exception.message}", Toast.LENGTH_SHORT).show()
         }
     }
 }
