fbb67dfd0fd618d00ee4db0e3d39d1baaf094ff5
Piotr Warnowski <162305236+PKWPiotrekwar@users.noreply.github.com>
2024-06-13 18:55:49 +0200

    naprawa ActiveWorkout

--- GymApp/app/src/main/java/com/example/testfirebase/ActiveWorkout.kt
+++ GymApp/app/src/main/java/com/example/testfirebase/ActiveWorkout.kt
@@ -172,35 +172,37 @@ class ActiveWorkout : AppCompatActivity() {
     private fun printExercisesFromDocument(documentId: String) {
         val collectionReference = FirebaseFirestore.getInstance().collection("WorkoutPlans")
         val documentReference = collectionReference.document(documentId)
-
         documentReference.get()
             .addOnSuccessListener { documentSnapshot ->
                 if (documentSnapshot.exists()) {
                     val documentData = documentSnapshot.data
                     if (documentData != null) {
                         val exercisesLayout = findViewById<LinearLayout>(R.id.exercisesLayout)
-                        val exerciseList = mutableListOf<Triple<String, String, String?>>()
+                        val exercisesMap = documentData["Exercises"] as? Map<String, String>
 
-                        for ((fieldName, fieldValue) in documentData) {
-                            if (fieldName.startsWith("Exercise")) {
-                                // Extract the exercise name and reps from the field value
-                                val exerciseData = fieldValue.toString().split("=")
-                                val exerciseName = exerciseData[0].trim().removePrefix("{")
-                                val exerciseReps = if (exerciseData.size > 1) exerciseData[1].trim().removeSuffix("}") else ""
-                                exerciseList.add(Triple(fieldName, exerciseName, exerciseReps))
+                        if (exercisesMap != null) {
+                            // Handle map structure
+                            for ((exerciseName, exerciseReps) in exercisesMap) {
+                                val exerciseLayout = createExerciseLayout(exerciseName, exerciseReps, R.drawable.style_unchecked)
+                                exercisesLayout.addView(exerciseLayout)
                                 exerciseCheckedState[exerciseName] = false // Initialize checked state
                             }
-                        }
-
-                        exerciseList.sortBy { it.first.substringAfter("Exercise ").toInt() }
-
-                        for ((fieldName, exerciseName, exerciseReps) in exerciseList) {
-                            val backgroundRes = if (exerciseReps != null) R.drawable.style_unchecked else R.drawable.style_checked
-                            val exerciseLayout = exerciseReps?.let {
-                                createExerciseLayout(exerciseName,
-                                    it, backgroundRes)
+                        } else {
+                            // Handle separate fields structure
+                            val exerciseList = mutableListOf<Triple<String, String, String?>>()
+                            for ((fieldName, fieldValue) in documentData) {
+                                if (fieldName.startsWith("Exercise")) {
+                                    val exerciseName = fieldValue.toString().trim().removePrefix("{").removeSuffix("}")
+                                    exerciseList.add(Triple(fieldName, exerciseName, null))
+                                    exerciseCheckedState[exerciseName] = false // Initialize checked state
+                                }
                             }
-                            exercisesLayout.addView(exerciseLayout)
+                            exerciseList.sortBy { it.first.substringAfter("Exercise ").toInt() }
+
+                            for ((fieldName, exerciseName, _) in exerciseList) {
+                                val exerciseLayout = createExerciseLayout(exerciseName, "", R.drawable.style_unchecked)
+                                exercisesLayout.addView(exerciseLayout)
+                            }
                         }
                     } else {
                         Log.d("TAG", "Brak danych w dokumencie.")
@@ -213,6 +215,8 @@ class ActiveWorkout : AppCompatActivity() {
                 Log.d("TAG", "Błąd podczas pobierania dokumentu: $exception")
             }
     }
+
+
 
     private fun createExerciseLayout(exerciseName: String, exerciseReps: String, backgroundRes: Int): LinearLayout {
         return LinearLayout(this).apply {
